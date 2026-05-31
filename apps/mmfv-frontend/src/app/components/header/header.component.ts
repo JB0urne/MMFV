bd3bea630c4391f2654e794cf2076aa5efb63742
JB0urne <tgdev@quantentunnel.de>
2026-05-31 17:13:33 +0200

    refactor: remove submenu placeholder from header
    simplify logo placeholder
    
    sqush

--- apps/mmfv-frontend/src/app/components/header/header.component.ts
+++ apps/mmfv-frontend/src/app/components/header/header.component.ts
@@ -8,14 +8,4 @@ import { CommonModule } from '@angular/common';
     templateUrl: './header.component.html',
     styleUrl: './header.component.css',
 })
-export class HeaderComponent {
-    showSortierenMenu = false;
-
-    toggleSortierenMenu() {
-        this.showSortierenMenu = !this.showSortierenMenu;
-    }
-
-    selectSubmenuItem(item: string) {
-        console.log('Selected submenu item:', item);
-    }
-}
+export class HeaderComponent {}
